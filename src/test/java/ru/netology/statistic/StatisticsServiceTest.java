e79c19c35f0626eb536bcba3ad21c06a11ce9a38
Denis Putyatin <denb6377@gmail.com>
2022-05-29 17:46:28 +0300

    add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsServiceTest {
 
@@ -10,8 +10,20 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 12, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxInTheMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 50, 12};
+        long expected = 50;
 
         long actual = service.findMax(incomesInBillions);
 
